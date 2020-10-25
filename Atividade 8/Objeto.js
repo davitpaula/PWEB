85322a6a8459f434c3101a4d2d101482834727c9
davitpaula <70110489+davitpaula@users.noreply.github.com>
2020-10-25 14:27:12 -0300

    Add files via upload

---
+++ Atividade 8/Objeto.js
@@ -0,0 +1,18 @@
+var Aluno1 = {};
+var Registro = "RA";
+var Identidade = "Nome";
+Aluno1["RA"]="0030481911007";
+Aluno1["Nome"]="Davi Teixeira de Paula";
+//Primeiro jeito
+
+var Aluno2 = {
+    RA: "0030481911007",
+    Nome: "Davi Teixeira de Paula"
+};
+
+//Segundo jeito
+
+var Aluno3 = new Object();
+Aluno3.RA="0030481911007";
+Aluno3.Nome="Davi Teixeira de Paula";
+//Terceiro jeito
