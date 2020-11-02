d5ac86d8008963b3c82b878e9f7f2928882b11d1
davitpaula <70110489+davitpaula@users.noreply.github.com>
2020-11-02 16:19:48 -0300

    Add files via upload

---
+++ Portfolio/JS/Peixe.js
@@ -0,0 +1,12 @@
+function mClick(obj)
+{
+obj.src="IMG/fish4.png";
+var t = document.getElementById("cabeca");
+t.innerHTML = "Peixe";
+}
+function mOut2(obj)
+{
+obj.src="IMG/fish3.png";
+var t = document.getElementById("cabeca");
+t.innerHTML = "Peixe";
+}
---
+++ Portfolio/JS/cursos&trabalhos.js
@@ -0,0 +1,11 @@
+function mOver(obj)
+{
+obj.style.background="#000000";
+obj.innerHTML="Nenhum";
+}
+
+function mOut(obj)
+{
+obj.style.background="#FFFFFF";
+obj.innerHTML="Cursos e trabalhos realizados(passe o mouse em cima)";
+}
